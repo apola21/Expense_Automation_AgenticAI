259be709f3a9be9fa9fdeb7ee7afb9efaea7a1a8
Arun <kchandran@beastute.com>
2025-10-14 20:46:52 +0530

    Create config_AGENT.py

---
+++ config_AGENT.py
@@ -0,0 +1,11 @@
+LOG_PATH="/"
+SMTP_HOST = 'imap.gmail.com'
+SMTP_USER = ''
+SMTP_PASSWORD=""
+SMTP_MAIL_SERVER = "smtp.gmail.com"
+SMTP_MAIL_PORT = 587
+OCI_CONFIG = '/root/.oci/config'
+OCI_BUCKET_NAME = ''
+OCI_TARGET_BUCKET_NAME = ''
+
+GEMINI_API_KEY = "AIzaSyDpJE0dXs8T005LzrR0Luc5XrVShUPq_e8"
